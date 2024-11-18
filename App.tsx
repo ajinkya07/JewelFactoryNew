06e4ebf0d32292489294cd3c707076f4d96283a7
ajinkya07 <ajinkyapalv@gmail.com>
2024-11-18 23:41:48 +0530

    folder structure in progress

--- App.tsx
+++ App.tsx
@@ -83,9 +83,6 @@ function App(): React.JSX.Element {
           <Section title="See Your Changes">
             <ReloadInstructions />
           </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
           <Section title="Learn More">
             Read the docs to discover what to do next:
           </Section>
---
+++ src/utils/strings.ts
@@ -0,0 +1,24 @@
+export const strings = {
+    appName: 'Jewel Factory',
+    appSubName: '',
+    login: 'Log In',
+    forgotPassText: 'Forgot Login Details?',
+    forgotPassLink: 'Get Help',
+    dontHaveAccount: "Don't have an account?",
+    signUp: 'Sign Up',
+    invalidEmail: 'Please enter correct email',
+    invalidPassword:
+      'Password must be atleast six character long, contain one special character and one numeric',
+    globalErrorMsg: 'Something went wrong',
+    serverFailedMsg:
+      'We are unable to connect at this moment.\nPlease try again later.',
+    categoryDesigns: 'Category',
+    seeMore: 'More',
+    latestDesigns: 'Latest',
+    bestSelling: 'Best',
+    diwaliCollection: 'Diwali Collection',
+    submit: 'Submit',
+    scannedCodePH: 'Code you have scanned will appear here..',
+    close: 'close',
+  };
+  
